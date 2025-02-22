bc8c068c8fedff3299433972df95588ff6d7ff11
Pavanivemuri12 <153835795+Pavanivemuri12@users.noreply.github.com>
2025-02-22 22:52:23 +0530

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,18 @@
+import Banner from "./components/Banner/Banner";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+
+
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden bg-white text-dark">
+    
+      <Hero/>
+      <Services/>
+      <Banner/>
+    </main>
+  );
+}
+
+export default App
